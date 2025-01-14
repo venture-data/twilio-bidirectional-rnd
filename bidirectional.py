446fcd1c7f4462c51ee7c5622ae0bb59edbd332a
101ammarAh <ammarah5683@gmail.com>
2025-01-14 12:31:02 +0500

    added tonality

--- bidirectional.py
+++ bidirectional.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
-from fastapi.responses import Response
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import websockets
 import asyncio
 import os
@@ -18,7 +17,8 @@ SYSTEM_MESSAGE = (
     "You are reaching out to a customer named Nikolai regarding an upcoming Plumbing Maintenance due in February. "
     "Greet him politely, confirm his availability, and book his appointment at a suitable date/time. "
     "If he asks questions or prefers a different date, respond helpfully and professionally. "
-    "Keep the conversation natural and polite. Make sure to keep Arizona accent and add some filler words like 'uh', 'um', 'like', 'you know' to sound more natural."
+    "Keep the conversation natural and polite and speak fast. "
+    "Make sure to keep Arizona accent and add some filler words like 'uh', 'um', 'like', 'you know', etc. to sound more natural."
 )
 VOICE = "verse"
 PORT = int(os.getenv("PORT", 5050))
