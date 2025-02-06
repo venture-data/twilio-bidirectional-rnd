8a719cfde494a55ec5d0a7597e724fcef4620cc8
V12Hero <ammar.ahmad20@yahoo.com>
2025-02-06 17:14:12 +0500

    added various volumes

--- ElevenLabs/main.py
+++ ElevenLabs/main.py
@@ -260,7 +260,7 @@ async def handle_media_stream(websocket: WebSocket):
     print("WebSocket connection opened")
 
     audio_interface = TwilioAudioInterface(websocket)
-    audio_interface.load_background_noise("background.ulaw")
+    audio_interface.load_background_noise("call-center-youtube-01.ulaw")
 
     local_call_sid = None
     conversation = None
